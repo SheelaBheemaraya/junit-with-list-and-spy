20086de43851a6681c92eb60b8deee9dc9a0a726
SheelaBheemaraya <sb3891604@gmail.com>
2022-01-17 13:53:44 +0530

    the concept of Argument matcher and spy

---
+++ unit-testing-list-and-spy/src/main/java/com/thbs/bt/unittestinglistandspy/UnitTestingListAndSpyApplication.java
@@ -0,0 +1,13 @@
+package com.thbs.bt.unittestinglistandspy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingListAndSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingListAndSpyApplication.class, args);
+	}
+
+}
---
+++ unit-testing-list-and-spy/src/test/java/com/thbs/bt/unittestinglistandspy/UnitTestingListAndSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thbs.bt.unittestinglistandspy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingListAndSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
